57a8679f5bbbd31ba31356a45bf0241fcd7f19c4
AlbertStephenGerenaCastellanos <algecass94@gmail.com>
2021-07-07 20:56:50 -0500

    Repaso de variables -  07/07/2021

---
+++ main.js
@@ -0,0 +1,24 @@
+nombreCompleto = `Albert Stephen Gerena Castellanos`;
+miEdad = 27;
+altura = 1.76;
+soyProfesor = false;
+soyEstudiante = true;
+
+baseTrinagulo = 2;
+alturaTrinagulo = 0.5444456786578785678678;
+areaTriangulo = (baseTrinagulo*alturaTrinagulo)/2;
+
+numerosPares = [2,4,6,8,10,12];
+
+
+console.log(nombreCompleto, typeof(nombreCompleto));
+console.log(miEdad, typeof(miEdad));
+console.log(soyProfesor,typeof(soyProfesor));
+console.log(soyEstudiante,typeof(soyEstudiante));
+console.log(altura,typeof(altura));
+console.log(areaTriangulo,typeof(areaTriangulo));
+console.log(numerosPares,typeof(numerosPares));
+
+
+// alert()
+// document.write();
